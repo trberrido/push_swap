accf154f9da8f072ab3b95ce17397f19004a5baf
Thibault BERRIDO <thberrid@e2r2p10.42.fr>
2019-04-14 14:25:11 +0200

    libft add memswap

---
+++ libft/ft_memswap.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memswap.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/14 14:07:58 by thberrid          #+#    #+#             */
+/*   Updated: 2019/04/14 14:23:43 by thberrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_memswap(void *addr_a, void *addr_b, size_t size)
+{
+	void	*tmp;
+
+	 if ((tmp = ft_memalloc(sizeof(char) * size)))
+	 {
+		ft_memcpy(tmp, addr_a, size);
+		ft_memcpy(addr_a, addr_b, size);
+		ft_memcpy(addr_b, tmp, size);
+		ft_memdel(&tmp);
+	 }
+	 return (NULL);
+}

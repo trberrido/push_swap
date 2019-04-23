f7b46132f8e865b358bfb0a911c4cbbdc5d11f48
Thibault BERRIDO <thberrid@e1r11p18.42.fr>
2019-04-23 14:45:33 +0200

    set stackdel & print in separate files

--- ft_stackdel.c
+++ ft_stackdel.c
@@ -6,7 +6,7 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 14:31:05 by thberrid          #+#    #+#             */
-/*   Updated: 2019/04/23 14:31:17 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/04/23 14:35:39 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void			ft_stacksdel(t_list **stack_a, t_list **stack_b, t_list **ops)
 {
 	if (stack_a && *stack_a)
-		ft_lstdel(stack_a, &ft_bzero);
+		ft_lstdel(stack_a, &ft_memerase);
 	if (stack_b && *stack_b)
-		ft_lstdel(stack_b, &ft_bzero);
+		ft_lstdel(stack_b, &ft_memerase);
 	if (ops && *ops)
-		ft_lstdel(ops, &ft_bzero);
+		ft_lstdel(ops, &ft_memerase);
 }
 
--- ft_stackfill.c
+++ ft_stackfill.c
@@ -6,21 +6,11 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 14:55:28 by thberrid          #+#    #+#             */
-/*   Updated: 2019/04/19 18:12:18 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/04/23 14:43:26 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
-
-void			ft_stacksdel(t_list **stack_a, t_list **stack_b, t_list **ops)
-{
-	if (stack_a && *stack_a)
-		ft_lstdel(stack_a, &ft_bzero);
-	if (stack_b && *stack_b)
-		ft_lstdel(stack_b, &ft_bzero);
-	if (ops && *ops)
-		ft_lstdel(ops, &ft_bzero);
-}
 
 static int		ft_checktwins(t_list *stack, int value)
 {
@@ -38,21 +28,6 @@ static int		ft_checktwins(t_list *stack, int value)
 		current = current->next;
 	}
 	return (1);
-}
-
-void			ft_stackprint(t_list *stack, char name)
-{
-	ft_putchar(name);
-	ft_putendl(":");
-	if (!stack)
-		ft_putendl("empty stack");
-	while (stack)
-	{
-		ft_putnbr(((t_plate *)stack->content)->value);
-		ft_putendl("");
-		stack = stack->next;
-	}
-	ft_putendl("");
 }
 
 static t_list	*ft_addplate(t_list **stack, t_plate *newplate)

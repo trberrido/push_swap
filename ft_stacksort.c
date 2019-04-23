c9245d54d37ee9df74d111c4bef264a1b8a4014d
Thibault BERRIDO <thberrid@e1r11p18.42.fr>
2019-04-23 16:09:03 +0200

    check if list is sorted

--- ft_stacksort.c
+++ ft_stacksort.c
@@ -6,7 +6,7 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 18:19:05 by thberrid          #+#    #+#             */
-/*   Updated: 2019/04/19 18:45:52 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/04/23 15:13:57 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,13 @@ t_list		*ft_stacksort(t_list **stack_a, t_list **stack_b, t_list **ops)
 	};
 
 	i = 0;
+	ft_stacksort_pre(stack_a, stack_b, ops);
+	if (ft_stacksort_check(stack_a, stack_b))
+		return (ops);
 	stack_len = ft_lstlen(*stack_a);
 	while (stack_len > algos[i].limit && i + 1 < 2)
 		i += 1;
-	return (algos[i].fn(stack_a, stack_b, ops));
+	algos[i].fn(stack_a, stack_b, ops);
+	ft_stacksort_post(stack_a, stack_b, ops);
+	return (*ops);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 20:28:10 by thberrid          #+#    #+#             */
-/*   Updated: 2019/04/23 15:03:34 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/04/23 15:16:02 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,13 @@ int		main(int ac, char **av)
 		ft_putendl_fd("Error", 2);
 	else if (!ft_stackfill(ac, av, &stack_a))
 		ft_putendl_fd("Error", 2);
-	else if (!ft_stacksort(&stack_a, &stack_b, &stack_ops))
-		ft_putendl_fd("Error", 2);
-	else
-		ft_lstiter(stack_ops, ft_opwrite);
+	else if (!ft_stacksort_check(stack_a, stack_b))
+	{
+		if (!ft_stacksort(&stack_a, &stack_b, &stack_ops))
+			ft_putendl_fd("Error", 2);
+		else
+			ft_lstiter(stack_ops, ft_opwrite);
+	}
 	ft_stacksdel(&stack_a, &stack_b, &stack_ops);
 	return (0);
 }

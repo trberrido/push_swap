40ea7a6c496e64042c2e0833b16c5a2c02684a3c
Thibault BERRIDO <thberrid@e1r11p18.42.fr>
2019-04-23 15:06:07 +0200

    sorted check in separate file

---
+++ ft_stacksort_check.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stacksort_check.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/23 14:51:34 by thberrid          #+#    #+#             */
+/*   Updated: 2019/04/23 15:04:57 by thberrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <push_swap.h>
+
+int		ft_stacksort_check(t_list *stack_a, t_list *stack_b)
+{
+	if (!ft_countbreaks(stack_a) && !stack_b)
+		return (1);
+	return (0);
+}

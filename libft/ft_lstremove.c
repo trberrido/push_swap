b3f92a4b42e251cf5ed73a18561b46328726898a
Thibault BERRIDO <thberrid@e1r5p9.42.fr>
2019-04-24 14:12:20 +0200

    add libft / lst remove

---
+++ libft/ft_lstremove.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstremove.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/23 22:55:35 by thberrid          #+#    #+#             */
+/*   Updated: 2019/04/23 23:04:22 by thberrid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	ft_lstremove(t_list **list, t_list *to_del, void (*del)(void *, size_t))
+{
+	t_list	*current;
+
+	current = *list;
+	while (current)
+	{
+		if (current == to_del)
+			break ;
+		current = current->next;
+	}
+	current = current->next;
+	ft_lstdelone(&to_del, ft_memerase);
+}

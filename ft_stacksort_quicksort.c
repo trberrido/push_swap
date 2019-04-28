eff00d011ec8df0ee695e3010d5a57c39d613dc8
Thibault BERRIDO <thberrid@e1r1p7.42.fr>
2019-04-28 14:42:28 +0200

    minor edit: clean to enable test of FN getmedian

--- ft_stacksort_quicksort.c
+++ ft_stacksort_quicksort.c
@@ -6,23 +6,38 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/26 20:07:25 by thberrid          #+#    #+#             */
-/*   Updated: 2019/04/26 21:12:42 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/04/28 14:32:30 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-static int		ft_getmedian(*stack)
+int				ft_getmedian(t_list *stack, int len)
 {
-	long	sum;
+	t_list	*current_lst;
+	int		current_value;
+	int		prev_min;
+	int		i;
+	int		min;
 
-	sum = 0;
-	while (stack)
+	i = 0;
+	prev_min = FT_INTMIN;
+	while (i < (len + 1) / 2)
 	{
-		sum += ((t_plate *)stack->content)->value;		
-		stack = stack->next;
+		ft_putendl("-");
+		current_lst = stack;
+		min = FT_INTMAX;
+		while (current_lst)
+		{
+			current_value = ((t_plate *)current_lst->content)->value;
+			if (current_value > prev_min && current_value < min)
+				min = current_value;
+			current_lst = current_lst->next;
+		}
+		prev_min = min;
+		i += 1;
 	}
-	return (sum / ft_lstlen(stack));
+	return (min);
 }
 
 static t_list	*ft_sliceit(t_list **s_a, t_list **s_b, t_list **ops, int len)
@@ -30,19 +45,19 @@ static t_list	*ft_sliceit(t_list **s_a, t_list **s_b, t_list **ops, int len)
 	int		median;
 	int		i;
 
-	median = ft_getmedian(*s_a);
+	median = ft_getmedian(*s_a, ft_lstlen(*s_a));
 	i = 0;
 	while (i < len)
 	{
 		if (((t_plate *)(*s_a)->content)->value < median)
 		{
-			ft_push(s_a, s_b, ops);
+			ft_push(s_a, s_b);
 			if (!ft_opadd(ops, "pb"))
 				return (NULL);
 		}
 		else
 		{
-			ft_rotate(s_a, s_b, ops);
+			ft_rotate(s_a);
 			if (!ft_opadd(ops, "ra"))
 				return (NULL);
 		}
@@ -60,13 +75,7 @@ t_list			*ft_quicksort(t_list **stack_a, t_list **stack_b, t_list **ops)
 	{
 		if (!ft_sliceit(stack_a, stack_b, ops, len))
 			return (NULL);
-		if (!ft_quicksort(stack_a, stack_b, ops)
-			return (NULL);
-		if (!ft_halfitback(stack_a, stack_b, ops, len))
-			return (NULL);
 		len = ft_lstlen(*stack_a);
 	}
-	if (!ft_minibubble(stack_a, ops))
-		return (NULL);
-	return (*ops)
+	return (*ops);
 }

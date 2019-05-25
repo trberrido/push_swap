687135babaaeb934a286a8aa0a8ce84909d60f3e
Thibault BERRIDO <thberrid@e1r4p14.42.fr>
2019-05-25 18:49:44 +0200

    fix invalid read

--- libft/ft_memrealloc.c
+++ libft/ft_memrealloc.c
@@ -6,7 +6,7 @@
 /*   By: thberrid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 15:02:51 by thberrid          #+#    #+#             */
-/*   Updated: 2019/05/25 17:05:21 by thberrid         ###   ########.fr       */
+/*   Updated: 2019/05/25 18:41:53 by thberrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memrealloc(void *ptr, size_t size)
 
 	new = ft_memalloc(size);
 	if (new)
-		new = ft_memcpy(new, ptr, size);
+		new = ft_strcpy(new, ptr);
 	ft_memdel(&ptr);
 	return (new);
 }
